bc4a97669ec0d29583d980f710984e8198b2311a
Corey McCormick <coreymccor@live.com.au>
2023-08-26 21:48:18 +1000

    Restored the grid layout to advanced options

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -148,24 +148,20 @@
         <div class="collapse-title text-xl font-medium">
             Advanced options
         </div>
-        <div class="collapse-content flex flex-col gap-2">
-            <div class="flex flex-col md:flex-row items-center gap-2">
-                <label for="relationship" class="font-bold">Relationship</label>
-                <select class="select w-full" name="relationship" >
-                    <option selected>Partner</option>
-                    <option>Friend</option>
-                    <option>Family</option>
-                    <option>Acquaintance</option>
-                    <option>Colleague</option>
-                    <option>Other / General</option>
-                </select>
-            </div>
-            <div class="flex flex-col md:flex-row items-center gap-2">
-                <label for="quality" class="font-bold">Quality</label>
-                <div class="join w-full md:w-auto md:mx-auto">
-                    <input class="join-item btn grow border-primary md:px-8" type="radio" name="quality" aria-label="Helpful" value="helpful" checked />
-                    <input class="join-item btn grow border-primary md:px-8" type="radio" name="quality" aria-label="Stupid" value="stupid" />
-                </div>
+        <div class="collapse-content grid grid-cols-1 md:grid-cols-[25%_auto] gap-2 items-center">
+            <label for="relationship" class="font-bold">Relationship</label>
+            <select class="select w-full" name="relationship" >
+                <option selected>Partner</option>
+                <option>Friend</option>
+                <option>Family</option>
+                <option>Acquaintance</option>
+                <option>Colleague</option>
+                <option>Other / General</option>
+            </select>
+            <label for="quality" class="font-bold">Quality</label>
+            <div class="join w-full">
+                <input class="join-item btn grow border-primary md:px-8" type="radio" name="quality" aria-label="Helpful" value="helpful" checked />
+                <input class="join-item btn grow border-primary md:px-8" type="radio" name="quality" aria-label="Stupid" value="stupid" />
             </div>
         </div>
       </div>
