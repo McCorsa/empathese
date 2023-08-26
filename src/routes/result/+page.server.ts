e28fddfd1b405ff852f8e5a1a24d689cafde4744
Farzad Hayat <farzadhayat2002@gmail.com>
2023-08-26 18:49:46 +1000

    stupid mode now works for gpt-4

--- src/routes/result/+page.server.ts
+++ src/routes/result/+page.server.ts
@@ -14,61 +14,56 @@ export const actions = {
         const data = await request.formData();
         const yourMessage = data.get("yourMessage")?.toString();
         const theirMessage = data.get("theirMessage")?.toString();
-        const relationship = data.get("relationship")?.toString();
-        // const tone = data.get("tone")?.toString();
-        const responseQuality = data.get("quality")?.toString();
-        // const relationship = "girlfriend";
-        const tone = "casual";
-        // const responseQuality = "helpful";
+        const relationship = data.get("relationship")?.toString().toLowerCase();
+        const responseQuality = data.get("quality")?.toString().toLowerCase();
 
         let responseQualityString = `You are a helpful conversation companion who helps me understand my conversation with my ${relationship} and uncover what my ${relationship} really means when they says something to me.`;
         if (responseQuality == "stupid") {
-            responseQualityString = `You are a stupid conversation companion who gives me horrible, counterintuitive advice that completely misses the point.`;
+            responseQualityString = `You a friend who gives me horrible conversation advice. You have no clue what you're talking about and give answers that completely miss the point.`;
         }
 
-        const responseFormat = `"{
+        const responseFormat = `{
             translation: <your interpretation of their response as a string>,
             suggestions: [
-                <body of first suggested response as string>,
-                <body of second suggested response as string>,
-                <body of third suggested response as string>,
+                <first ${responseQuality} suggestion for a response as string>,
+                <second ${responseQuality} suggestion for a response as string>,
+                <third ${responseQuality} suggestion for a response as string>,
             ]
-        }"`;
-
-        // The tone of the conversation is ${tone}, so cater your response to take into account the ${tone} tone.
+        }`;
 
         // Setting up the scenario
         const systemMessage =
         `${responseQualityString}
-        I will give you a scenario of what I said and what my ${relationship} said.
-        Give a response including an interpretation of what they said in 32 words maximum from your point of view speaking to me,
-        along with three suggestions for possible responses I could say to them from my point of view.
+        I will give you a scenario of what I said and what they said.
+        Give a response including an interpretation of what they said from your point of view speaking to me, along with three suggestions for possible responses I could say to them from my point of view.
         Respond in the following JSON object format:
 
         ${responseFormat}
         
         Don't give explanation or extra boilerplate around your response. Only include the JSON format above in your response.`;
         console.log(systemMessage);
+
         // API call
         const chatCompletion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
+            // model: "gpt-4-0314",
             messages: [
                 {"role": "system", "content": systemMessage},
-                {"role": "user", "content": `I said to my ${relationship}: ${yourMessage}`},
-                {"role": "user", "content": `My ${relationship} said back to me: ${theirMessage}`},
-                {"role": "user", "content": `This person's relationship to me is: ${relationship}`},
-                // {"role": "user", "content": `Tone of the conversation: ${tone}`},
+                {"role": "user", "content": `I said: ${yourMessage}`},
+                {"role": "user", "content": `They said: ${theirMessage}`},
+                {"role": "user", "content": `Relationship: ${relationship}`},
             ],
         });
     
+        // Parsing the response
         const response = chatCompletion.choices[0].message.content || "{}";
+        console.log(response);
         const parsed = JSON.parse(response);
         
         return {
             yourMessage,
             theirMessage,
             relationship,
-            // tone,
             responseQuality,
             translation: parsed.translation,
             suggestions: parsed.suggestions,
