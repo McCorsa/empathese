d27f1850b1cdd9689519ac208d0a0bbc4a62d5ac
Farzad Hayat <farzadhayat2002@gmail.com>
2023-08-26 11:30:45 +1000

    Added OpenAI npm package and mock API return data.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,13 @@
 </script>
 
 <h1 class="text-3xl font-black">Welcome to SvelteKit</h1>
-<p>My message is "{data.message}"</p>
+<!-- <p>My message is "{data.message}"</p> -->
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/" class="btn btn-primary">test</a>
+
+<br>
+<p>My message is "{data.translation}"</p>
+{#each data.suggestions as suggestion}
+    <p>{suggestion.title}: {suggestion.response}</p>
+{/each}
+<br>
