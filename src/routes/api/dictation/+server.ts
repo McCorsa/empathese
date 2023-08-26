e064b27bbfdf4125ec764891437f365352aa8292
Farzad Hayat <farzadhayat2002@gmail.com>
2023-08-26 20:37:59 +1000

    openai whisper

--- src/routes/api/dictation/+server.ts
+++ src/routes/api/dictation/+server.ts
@@ -1,7 +1,21 @@
 import {error} from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
+import {OpenAI} from "openai";
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    organization: process.env.OPENAI_ORG_ID
+});
 
 export const POST: RequestHandler = async ({ url, request }) => {
-    const audioUrl = await request.text()
-    return new Response(String(audioUrl));
+
+    const response = await openai.audio.transcriptions.create({
+        model: 'whisper-1',
+        file: fs.createReadStream('sample.mp3')
+    });
+    
+    return new Response(String(response.text));
 }
