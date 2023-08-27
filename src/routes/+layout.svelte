a99d66c7802d0787d644eb4c003e319354b9ff7e
Farzad Hayat <farzadhayat2002@gmail.com>
2023-08-27 11:50:00 +1000

    Fixed screen width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bg-base-100 min-h-screen p-2 flex flex-col">
-    <main class="max-w-screen-md mx-auto flex-grow h-full bg-base-300 p-6 rounded-lg">
+    <main class="max-w-screen-md mx-auto flex-grow h-full w-full bg-base-300 p-6 rounded-lg">
         <slot />
     </main>
 </div>
