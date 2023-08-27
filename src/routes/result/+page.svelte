b644d7c2078e978ab7f8d9f373469a0a5c5e7730
Corey McCormick <coreymccor@live.com.au>
2023-08-27 12:32:13 +1000

    Changes to play buttons

--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -12,7 +12,7 @@
         if (playing !== text) {
             playing = text;
             const utterThis = new SpeechSynthesisUtterance(text);
-            utterThis.voice = speechSynthesis.getVoices()[1];
+            utterThis.voice = speechSynthesis.getVoices()[2];
             speechSynthesis.cancel();
             speechSynthesis.speak(utterThis);
 
@@ -38,11 +38,11 @@
     <button on:click={() => speak(form?.translation)} class={`btn ${playing === form?.translation ? 'btn-primary' : 'btn-neutral'}`}>
         {#if playing !== form?.translation}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
             </svg>
         {:else}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
             </svg>
         {/if}
     </button>
@@ -53,19 +53,8 @@
 
 <div class="flex flex-col gap-4">
     {#each form?.suggestions as suggestion, i}
-    <div class="flex items-center justify-between space-x-4">
-        <SuggestedResponse index={i + 1} suggestion={suggestion} />
-        <button on:click={() => speak(suggestion)} class={`btn ${playing === suggestion ? 'btn-primary' : 'btn-neutral'}`}>
-            {#if playing !== suggestion}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-                </svg>
-            {:else}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-                </svg>
-            {/if}
-        </button>
-    </div>    
+        <div class="flex items-center justify-between space-x-4">
+            <SuggestedResponse index={i + 1} suggestion={suggestion} playing={playing} onspeak={() => speak(suggestion)} />
+        </div>    
     {/each}
 </div>
